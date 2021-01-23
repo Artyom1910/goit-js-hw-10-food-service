4b370eff646d1abc1ab15ea5d85cb51c25597bd5
Artyom Kupits <kup1910@gmail.com>
2021-01-23 19:24:09 +0200

    1

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import itemsTemplate from './menu.hbs';
+import items from './menu.json';
+import styles from './styles.css';
+
+
+
+const markup = itemsTemplate(items);
+
+
+
+const menuRef = document.querySelector('.js-menu');
+menuRef.insertAdjacentHTML('afterbegin', markup);
+
+//  =Theme==
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const checkboxRef = document.querySelector('#theme-switch-toggle');
+const bodyRef = document.querySelector('body');
+
+bodyRef.classList.add('light-theme');
+saveTheme();
+
+function themeSwitcher(event) {
+  if (event.target.checked) {
+    bodyRef.classList.remove('light-theme');
+    bodyRef.classList.add('dark-theme');
+    localStorage.setItem('Theme', 'dark-theme');
+  } else {
+    bodyRef.classList.remove('dark-theme');
+    bodyRef.classList.add('light-theme');
+    localStorage.setItem('Theme', 'light-theme');
+  }
+}
+
+checkboxRef.addEventListener('change', themeSwitcher);
+
+function saveTheme() {
+  const getTheme = localStorage.getItem('Theme');
+  if (getTheme === 'dark-theme') {
+    checkboxRef.checked = true;
+    bodyRef.classList.add('dark-theme');
+  }
+}
